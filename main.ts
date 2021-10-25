b94a62b512c1230d747c3ffd58609da8c0ddfb3e
Bobeff123 <93154498+Bobeff123@users.noreply.github.com>
2021-10-25 18:20:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
-let myCorg = corgio.create(SpriteKind.Player)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    myCorg.addToScript("bark")
+    myCorg.bark()
+    mySprite.destroy()
+    pause(2000)
+    game.splash("YAY! You now leveled up press up to jump.")
+    myCorg.verticalMovement()
+})
+let mySprite: Sprite = null
+let myCorg: Corgio = null
+myCorg = corgio.create(SpriteKind.Player)
 pause(1000)
 game.splash("Hey, WAKE UP!")
-myCorg.updateSprite()
+myCorg.updateSprite(true)
 pause(500)
 game.splash("good morning press arrows to move ")
 myCorg.horizontalMovement()
 pause(5000)
 game.splash("Come to the bone")
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -24,6 +35,6 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-mySprite.setPosition(128, 105)
+    `, SpriteKind.Food)
+mySprite.setPosition(132, 114)
 info.setScore(0)
