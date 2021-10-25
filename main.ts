f1e83d230b5577e5b2c53c46042d7ef312f8743f
Bobeff123 <93154498+Bobeff123@users.noreply.github.com>
2021-10-25 18:42:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,75 @@
+namespace SpriteKind {
+    export const food2 = SpriteKind.create()
+    export const food3 = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.food2, function (sprite, otherSprite) {
+    mySprite3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        . . . . 1 1 1 1 1 1 1 1 1 . . . 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.food3)
+    mySprite3.setPosition(69, 59)
+    mySprite2.destroy()
+    mySprite4 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 5 5 . . f d . . . 
+        . . . . . . f 4 f . . f . d . . 
+        . . . . . . 4 4 4 . . f d . d . 
+        . . . . . 5 5 2 5 5 . f . d . . 
+        . . . . 4 2 2 2 2 2 4 f d . . . 
+        . . . . . 5 2 2 2 5 . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . . 2 . 2 . . . . . . . 
+        . . . . . . 2 . 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    myCorg.addToScript("bark")
-    myCorg.bark()
     mySprite.destroy()
-    pause(2000)
     game.splash("YAY! You now leveled up press up to jump.")
-    myCorg.verticalMovement()
+    myCorg.verticalMovement(true)
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        . . . . 1 1 1 1 1 1 1 1 1 . . . 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . 1 1 1 . . . . . . . . . 1 1 1 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.food2)
+    mySprite2.setPosition(74, 95)
 })
+let mySprite4: Sprite = null
+let mySprite2: Sprite = null
+let mySprite3: Sprite = null
 let mySprite: Sprite = null
 let myCorg: Corgio = null
 myCorg = corgio.create(SpriteKind.Player)
