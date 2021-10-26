26e86ff392c61b4fe90c82d7c6309a89811fd5d7
Bobeff123 <93154498+Bobeff123@users.noreply.github.com>
2021-10-26 13:32:17 -0500

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,2 +1,120 @@
+@namespace
+class SpriteKind:
+    food2 = SpriteKind.create()
+    food3 = SpriteKind.create()
+
+def on_on_overlap(sprite, otherSprite):
+    global myEnemy
+    info.change_score_by(1)
+    mySprite3.destroy(effects.spray, 500)
+    myEnemy = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . 2 . . . . 2 . . . . . 
+                    . . . . . 2 . . . . . 2 . . . . 
+                    . . . . . . . . . . . 2 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . 2 2 . . . . . . . 
+                    . . . . 2 2 2 . . 2 2 . . . . . 
+                    . . . . 2 . . . . . 2 . . . . . 
+                    . . . 2 2 . . . . . . 2 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.enemy)
+    myEnemy.set_position(99, 115)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food3, on_on_overlap)
+
+def on_on_overlap2(sprite2, otherSprite2):
+    global mySprite3
+    info.change_score_by(1)
+    mySprite3 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+                    . . . . 1 1 1 1 1 1 1 1 1 . . . 
+                    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.food3)
+    mySprite3.set_position(73, 68)
+    mySprite2.destroy(effects.spray, 500)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food2, on_on_overlap2)
+
+def on_on_overlap3(sprite3, otherSprite3):
+    global mySprite2
+    info.change_score_by(1)
+    mySprite.destroy(effects.spray, 500)
+    game.splash("YAY! You now leveled up press up to jump.")
+    myCorg.vertical_movement(True)
+    mySprite2 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+                    . . . . 1 1 1 1 1 1 1 1 1 . . . 
+                    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . 1 1 1 . . . . . . . . . 1 1 1 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.food2)
+    mySprite2.set_position(74, 95)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap3)
+
+mySprite2: Sprite = None
+myEnemy: Sprite = None
+mySprite3: Sprite = None
+mySprite: Sprite = None
+myCorg: Corgio = None
 myCorg = corgio.create(SpriteKind.player)
+pause(1000)
 game.splash("Hey, WAKE UP!")
+myCorg.update_sprite(True)
+pause(500)
+game.splash("good morning press arrows to move ")
+myCorg.horizontal_movement()
+pause(5000)
+game.splash("Come to the bone")
+mySprite = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . 1 1 1 . . . . . . . . . 1 1 1 
+            . 1 1 1 . . . . . . . . . 1 1 1 
+            . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            . . . . 1 1 1 1 1 1 1 1 1 . . . 
+            . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+            . 1 1 1 . . . . . . . . . 1 1 1 
+            . 1 1 1 . . . . . . . . . 1 1 1 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.food)
+mySprite.set_position(132, 114)
+info.set_score(0)
--- main.ts
+++ main.ts
@@ -3,9 +3,32 @@ namespace SpriteKind {
     export const food3 = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.food3, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
     mySprite3.destroy(effects.spray, 500)
+    myEnemy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 2 . . . . 2 . . . . . 
+        . . . . . 2 . . . . . 2 . . . . 
+        . . . . . . . . . . . 2 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . 2 2 2 . . 2 2 . . . . . 
+        . . . . 2 . . . . . 2 . . . . . 
+        . . . 2 2 . . . . . . 2 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    myEnemy.setPosition(99, 115)
+    myEnemy.setVelocity(50, 0)
+    myEnemy.setBounceOnWall(true)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.food2, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.food2, function (sprite2, otherSprite2) {
+    info.changeScoreBy(1)
     mySprite3 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -27,7 +50,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.food2, function (sprite, otherSp
     mySprite3.setPosition(73, 68)
     mySprite2.destroy(effects.spray, 500)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite3, otherSprite3) {
     info.changeScoreBy(1)
     mySprite.destroy(effects.spray, 500)
     game.splash("YAY! You now leveled up press up to jump.")
@@ -52,7 +75,11 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
         `, SpriteKind.food2)
     mySprite2.setPosition(74, 95)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
 let mySprite2: Sprite = null
+let myEnemy: Sprite = null
 let mySprite3: Sprite = null
 let mySprite: Sprite = null
 let myCorg: Corgio = null
